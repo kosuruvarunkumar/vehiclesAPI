83ec4329920efc217b51b06a14db10cfd52c0975
kosuruvarunkumar <kosuruvarunkumar@gmail.com>
2021-07-17 15:27:14 +0530

    Updated PUT operation and added Test for PUT

--- vehicles-api/src/test/java/com/udacity/vehicles/api/CarControllerTest.java
+++ vehicles-api/src/test/java/com/udacity/vehicles/api/CarControllerTest.java
@@ -4,9 +4,7 @@ import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -104,7 +102,9 @@ public class CarControllerTest {
     @Test
     public void findCar() throws Exception {
         Car car = getCar();
-        mvc.perform(get(new URI("/cars/1"))).andExpect(status().isOk()).andExpect(jsonPath("$.details.model", is(car.getDetails().getModel())));
+        mvc.perform(get(new URI("/cars/1")))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.details.model", is(car.getDetails().getModel())));
     }
 
     /**
@@ -116,6 +116,24 @@ public class CarControllerTest {
         Car car = getCar();
         car.setId(1L);
         mvc.perform(delete(new URI("/cars/1"))).andExpect(status().isNoContent());
+    }
+
+    /**
+     * Tests the updation of a single car by ID.
+     * @throws Exception if the delete operation of a vehicle fails
+     */
+    @Test
+    public void updateCar() throws Exception {
+        Car car = getCar();
+        car.setCondition(Condition.NEW);
+        car.setId(1L);
+        mvc.perform(
+                put(new URI("/cars/1"))
+                        .content(json.write(car).getJson())
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .accept(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.details.model", is(car.getDetails().getModel())));
     }
 
     /**
